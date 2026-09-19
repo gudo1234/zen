7ceb8a94f7ddbca359c930db415b995c240276c4
gudo1234 <izumilitee@gmail.com>
2026-09-18 19:11:47 -0600

    Update import path for baileys module

--- dist/plugins/_noti.js
+++ dist/plugins/_noti.js
@@ -1,4 +1,4 @@
-//import { prepareWAMessageMedia, generateWAMessageFromContent } from "baileys"
+import { prepareWAMessageMedia, generateWAMessageFromContent } from "@whiskeysockets/baileys"
 
 export default {
     name: ["noti"],
